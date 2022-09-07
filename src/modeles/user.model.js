a5c603645def5eaa4741e63b776bf47843d20fe7
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-09-07 11:28:37 +0530

    commit

--- src/modeles/user.model.js
+++ src/modeles/user.model.js
@@ -1,8 +1,8 @@
 const mongoose =require("mongoose");
 
 const userSchema= new mongoose.Schema({
-    first_name:{type:String,required:true},
-    last_name:{type:String,required:true},
+    first_name:{type:String,required:false},
+    last_name:{type:String,required:false},
     profile_pic:[{type:String,required:true}]
 },{
     versionKey:false,
